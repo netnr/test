f81cbcfffcfb1a1f139f52bd971997a764a9e2f4
netnr <netnr@netnr.com>
2022-10-16 14:41:55 +0800

    m

---
+++ ndk/index.js
@@ -0,0 +1,13 @@
+[
+    {
+        namespace: "netnr",
+        items: [
+            {
+                name: "Model",
+                version: "1.0.0",
+                description: "C# 生成 Model",
+                name: "/ndx/netnr/netnr_model.js"
+            }
+        ]
+    }
+]
